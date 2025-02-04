ee06b24d561cdc1d099163d2a2d14c42b99ed254
Sherezade <Sherezade@172.16.50.152>
2025-02-04 19:21:52 +0100

    con php

--- index.php
+++ index.php
@@ -7,33 +7,33 @@
     <link rel="shortcut icon" href="../img/icono.jpg" type="image/x-icon" id="ico">
     <link rel="stylesheet" type="text/css" href="./css/estilo.css">
     <script src="./javascript/script.js"></script>
-
 </head>
 <body>
     <?php
+    // Aquí puedes agregar cualquier lógica PHP que necesites
     ?>
     <div>
         <header>
             <div class="contenedor-logo">
-                <img src="./img/fondomof.webp"class="logo">
+                <img src="./img/fondomof.webp" class="logo">
             </div>
             <div class="contenedor-nav">
                 <div class="buscador">
                     <form method="get">
                         <fieldset>
-                           <input type="text" id="input-buscar" placeholder="Buscar"  />
+                           <input type="text" id="input-buscar" name="buscar" placeholder="Buscar" />
                            <input id="boton-buscar" type="submit" value=" Buscar" />
                            <i class="icono-buscar"></i>
                         </fieldset>
                      </form>
                 </div>
                 
-                <nav >
+                <nav>
                     <div class="barra">
                         <ul>
-                            <li><a>Categorias</a></li>
-                            <li><a>Biblioteca</a></li>   
-                            <li><a>Perfil</a></li>
+                            <li><a href="#">Categorías</a></li>
+                            <li><a href="#">Biblioteca</a></li>   
+                            <li><a href="#">Perfil</a></li>
                         </ul>
                     </div>
                 </nav>
@@ -45,110 +45,35 @@
     </div>
     <section id="Novedades" class="contenedor-carrusel">
         <div class="carrusel">
-            <div class="flip-card">
-                <div class="flip-card-inner">
-                    <div class="flip-card-front">
-                        <img class="diapositiva" src="./img_portada/El Ascenso de un Imperio.jpeg" alt="Portada de El Ascenso de un Imperio" />
-                    </div>
-                    <div class="flip-card-back">
-                        <!-- Contenido adicional en el reverso de la tarjeta -->
-                    </div>
-                </div>
-            </div>
-    
-            <div class="flip-card">
-                <div class="flip-card-inner">
-                    <div class="flip-card-front">
-                        <img class="diapositiva" src="./img_portada/El circo de la noche.jpg" alt="Portada de El Circo de la Noche" />
-                    </div>
-                    <div class="flip-card-back">
-                        <!-- Contenido adicional en el reverso de la tarjeta -->
-                    </div>
-                </div>
-            </div>
-    
-            <!-- Repite la misma estructura para las demás imágenes -->
-            
-            <div class="flip-card">
-                <div class="flip-card-inner">
-                    <div class="flip-card-front">
-                        <img class="diapositiva" src="./img_portada/El Laberinto de los Susurros.jpeg" alt="Portada de El Laberinto de los Susurros" />
-                    </div>
-                    <div class="flip-card-back">
-                        <!-- Contenido adicional en el reverso de la tarjeta -->
-                    </div>
-                </div>
-            </div>
-            <div class="flip-card">
-                <div class="flip-card-inner">
-                    <div class="flip-card-front">
-                        <img class="diapositiva" src="./img_portada/Hestia.jpeg" alt="Portada de El Laberinto de los Susurros" />
-                    </div>
-                    <div class="flip-card-back">
-                        <!-- Contenido adicional en el reverso de la tarjeta -->
-                    </div>
-                </div>
-            </div>
-            <div class="flip-card">
-                <div class="flip-card-inner">
-                    <div class="flip-card-front">
-                        <img class="diapositiva" src="./img_portada/La Revelación.jpeg" alt="Portada de El Laberinto de los Susurros" />
-                    </div>
-                    <div class="flip-card-back">
-                        <!-- Contenido adicional en el reverso de la tarjeta -->
-                    </div>
-                </div>
-            </div>
-            <div class="flip-card">
-                <div class="flip-card-inner">
-                    <div class="flip-card-front">
-                        <img class="diapositiva" src="./img_portada/Las mil y una noches.jpeg" alt="Portada de El Laberinto de los Susurros" />
-                    </div>
-                    <div class="flip-card-back">
-                        <!-- Contenido adicional en el reverso de la tarjeta -->
-                    </div>
-                </div>
-            </div>
-            <div class="flip-card">
-                <div class="flip-card-inner">
-                    <div class="flip-card-front">
-                        <img class="diapositiva" src="./img_portada/Los Juicios de Salem.jpeg" alt="Portada de El Laberinto de los Susurros" />
-                    </div>
-                    <div class="flip-card-back">
-                        <!-- Contenido adicional en el reverso de la tarjeta -->
-                    </div>
-                </div>
-            </div>
-            <div class="flip-card">
-                <div class="flip-card-inner">
-                    <div class="flip-card-front">
-                        <img class="diapositiva" src="./img_portada/Reyes caídos.jpeg" alt="Portada de El Laberinto de los Susurros" />
-                    </div>
-                    <div class="flip-card-back">
-                        <!-- Contenido adicional en el reverso de la tarjeta -->
-                    </div>
-                </div>
-            </div>
-            <div class="flip-card">
-                <div class="flip-card-inner">
-                    <div class="flip-card-front">
-                        <img class="diapositiva" src="./img_portada/Todas las hadas del reino.jpeg" alt="Portada de El Laberinto de los Susurros" />
-                    </div>
-                    <div class="flip-card-back">
-                        <!-- Contenido adicional en el reverso de la tarjeta -->
-                    </div>
-                </div>
-            </div>
-            <div class="flip-card">
-                <div class="flip-card-inner">
-                    <div class="flip-card-front">
-                        <img class="diapositiva" src="./img_portada/Utopia.jpeg" alt="Portada de El Laberinto de los Susurros" />
-                    </div>
-                    <div class="flip-card-back">
-                        <!-- Contenido adicional en el reverso de la tarjeta -->
-                    </div>
-                </div>
-            </div>
+            <?php
+            // Array de imágenes y títulos
+            $libros = [
+                ["src" => "./img_portada/El Ascenso de un Imperio.jpeg", "alt" => "Portada de El Ascenso de un Imperio"],
+                ["src" => "./img_portada/El circo de la noche.jpg", "alt" => "Portada de El Circo de la Noche"],
+                ["src" => "./img_portada/El Laberinto de los Susurros.jpeg", "alt" => "Portada de El Laberinto de los Susurros"],
+                ["src" => "./img_portada/Hestia.jpeg", "alt" => "Portada de Hestia"],
+                ["src" => "./img_portada/La Revelación.jpeg", "alt" => "Portada de La Revelación"],
+                ["src" => "./img_portada/Las mil y una noches.jpeg", "alt" => "Portada de Las mil y una noches"],
+                ["src" => "./img_portada/Los Juicios de Salem.jpeg", "alt" => "Portada de Los Juicios de Salem"],
+                ["src" => "./img_portada/Reyes caídos.jpeg", "alt" => "Portada de Reyes caídos"],
+                ["src" => "./img_portada/Todas las hadas del reino.jpeg", "alt" => "Portada de Todas las hadas del reino"],
+                ["src" => "./img_portada/Utopia.jpeg", "alt" => "Portada de Utopia"],
+            ];
+
+            // Generar las tarjetas de los libros
+            foreach ($libros as $libro) {
+                echo '<div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <img class="diapositiva" src="' . $libro['src'] . '" alt="' . $libro['alt'] . '" />
+                            </div>
+                            <div class="flip-card-back">
+                                <!-- Contenido adicional en el reverso de la tarjeta -->
+                            </div>
+                        </div>
+                    </div>';
+            }
+            ?>
         </div>
     </section>
     
