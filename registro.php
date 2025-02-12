fdb4b5f224e3ba51804c0e6793a22e53c2dc5681
daw202 <larcol01@educantabria.es>
2025-02-12 17:37:18 +0100

    registro con comentarios

--- registro.php
+++ registro.php
@@ -30,44 +30,47 @@ and open the template in the editor.
         <hr>
         <p>¿Ya te has registrado?</p>
         <a href="login.php"><button class="login-button">Iniciar sesión</button></a><br><br>
-        <a href="index.php">Pulsa aqui</a>
-        
+        <p>Si no quieres registrar <a href="index.php">Pulsa aqui</a></p>
+
+
 
         <?php
         //obtener la fecha actual
         $fecha_actual = date("Y-m-d");
-       
-        $conexion =mysqli_connect("localhost", "root", "", "tintero") or die("No se puede conectar con el servidor o seleccionar la base de datos");
-        
+
+        //aqui hacemos la conexion a la base de datos
+        $conexion = mysqli_connect("localhost", "root", "", "tintero") or die("No se puede conectar con el servidor o seleccionar la base de datos");
+
+        //aqui comprobamos si se ha pulsado a enviar
         if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['enviar'])) {
-            $nombre =$_POST['nombre'];
-            $apellido=$_POST['apellido'];
-            $email=$_POST['email'];
-            $contrasena=$_POST['contrasena'];
-            
-            //aqui pasa un filtro si el correo es como tiene que ser si no es con las caracteristicas que se necesitan le sale un mensaje en rojo
-            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $nombre = $_POST['nombre'];
+            $apellido = $_POST['apellido'];
+            $email = $_POST['email'];
+            $contrasena = $_POST['contrasena'];
+
+            //aqui pasa un filtro para saber si tiene las caracteristicas que se necesitan del correo
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                
                 echo "<p style='color:red;'>Correo inválido</p>";
-            }else{
-                $sql= "INSERT INTO usuario( Nombre, Apellido, Correo_Electronico, Contrasena,Fecha_Registro) VALUES ('$nombre','$apellido','$email','$contrasena',' $fecha_actual' )";
-                if(mysqli_query($conexion,$sql)){
-                     echo "<p style='color:green;'>Usuario registrado con éxito</p>";
-                }else{
+            } else {
+                //aqui insertamos el usuario en la base de datos 
+                $sql = "INSERT INTO usuario( Nombre, Apellido, Correo_Electronico, Contrasena,Fecha_Registro) VALUES ('$nombre','$apellido','$email','$contrasena',' $fecha_actual')";
+                //aqui hacemos la conexion para introducirlo a la base de datos
+                if (mysqli_query($conexion, $sql)) {
+                    //si esta todo bien sale un mensaje en verde
+                    echo "<p style='color:green;'>Usuario registrado con éxito</p>";
+                } else {
+                    //si hay algun error sale un mensaje en rojo
                     echo "<p style='color:red;'>Error: " . mysqli_error($conn) . "</p>";
                 }
-                
             }
-            
-            
-            
-            
         }
-        
-        
-        
-        
-        
-        
+
+
+
+
+        // Cerrar la conexión
+        mysqli_close($conexion);
         ?>
     </body>
 </html>
