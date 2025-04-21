8712c9eff6fd30ef6a44545b702f5d448c8d9d8a
daw202 <larcol01@educantabria.es>
2025-04-21 17:30:01 +0200

    menus

---
+++ menuPlanPremium.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Tintero</title>
+        <link rel="shortcut icon" href="./img/icono.jpg" type="image/x-icon" id="ico">
+        <link rel="stylesheet" type="text/css" href="./css/estilo.css">
+        <!--CSS para los brillos del fondo-->
+        <link rel="stylesheet" type="text/css" href="./css/fondo_estrellas.css">
+        <script src="./javascript/script.js"></script>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
